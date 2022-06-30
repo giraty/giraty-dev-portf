2852d630ffe09ebc924ca26c6059ad8fc8ef04ca
octavianputra <octavianputra@yummycorp.com>
2022-06-30 22:47:47 +0700

    Add project page with SvelteKit and index page

---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,40 @@
+<script context="module">
+    import ProjectCard from '$lib/components/project-card.svelte'
+    import { client } from '$lib/graphql-client'
+    import { projectQuery} from '$lib/graphql-queries'
+
+    export const load = async () => {
+        const { projects } = await client.request(projectsQuery)
+    
+        return {
+            props: {
+                projects,
+            },
+        }
+
+    }
+
+</script>
+
+<script>
+    export let project
+</script>
+
+<svelte:head>
+    <title>My Portfoliio projects</title>
+</svelte:head>
+
+<h1 class="font-bold mb-20 text-center text-5xl">
+    Recent Projects by Me
+</h1>
+
+<div class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52">
+    {#each projects as { name, slug, description, image}, index}
+    <ProjectCard
+        {name}
+        {description}
+        url={image[0].url}
+        {index}
+        {slug} />
+    {/each}
+</div>
