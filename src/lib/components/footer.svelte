1368bb01ca7d7890599b730d6c244ea59079c980
octavianputra <octavianputra@yummycorp.com>
2022-07-02 14:43:41 +0700

    Add footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,25 @@
+<script>
+    import TwitterIcon from './twitter-icon.svelte'
+    import YouTubeIcon from './you-tube-icon.svelte'
+</script>
+
+<footer class="footer footer-center p-10 bg-primary text-primary-content rounded">
+    <div class="grid grid-flow-col gap-4">
+      <a class="link link-hover" href="/projects">Portfolio</a> 
+      <a class="link link-hover" href="/posts">Blog</a> 
+      <a class="link link-hover" href="/about">About</a> 
+    </div> 
+    <div>
+      <div class="grid grid-flow-col gap-4">
+        <a href="https://twitter.com">
+            <TwitterIcon />
+        </a>
+        <a href="https://youtube.com">
+            <YouTubeIcon />
+        </a> 
+      </div>
+    </div> 
+    <div>
+      <p>Copyright &copy; {'$new Date().getFullYear()}'} - All right reserved by me</p>
+    </div>
+  </footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import '../app.css';
+	import Footer from '$lib/components/footer.svelte'
+	import '../app.css'
 </script>
+
 
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
 
-
+<Footer />
